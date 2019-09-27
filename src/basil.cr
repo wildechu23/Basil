baed853550507201b49e1997cf4c13db55d51114
wildechu <wildechu23@gmail.com>
2019-09-27 00:31:12 -0400

    Frame + SDL require

---
+++ src/basil.cr
@@ -0,0 +1,14 @@
+# TODO: Write documentation for `Basil`
+require "sdl"
+require "sdl/image"
+require "sdl/mix"
+
+SDL.init(SDL::Init::VIDEO | SDL::Init::AUDIO); at_exit { SDL.quit }
+SDL::IMG.init(SDL::IMG::Init::PNG); at_exit { SDL::IMG.quit }
+SDL::Mix.open
+
+module Basil
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
