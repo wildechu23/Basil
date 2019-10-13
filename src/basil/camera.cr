35a29b218b562447021533efbf2ec98c5fc7042d
wildechu <wildechu23@gmail.com>
2019-10-13 01:55:52 -0400

    Fleshed out renderer.cr

---
+++ src/basil/camera.cr
@@ -0,0 +1,9 @@
+class Basil::Camera
+  property pos = Vec2(Float64).new 0.0,0.0
+  property posbuffer = Vec2(Float64).new 0.0,0.0
+  property smooth = false
+
+  def update
+    @pos = @posbuffer
+  end
+end
--- src/basil/renderer.cr
+++ src/basil/renderer.cr
@@ -1,3 +1,101 @@
-module Leafgem::Renderer
+module Basil::Renderer
+  @@fps = 60
+  @@renderer : SDL::Renderer?
+  @@window : SDL::Window?
 
+  @@camera = Basil::Camera.new
+
+  @@scale = 1
+
+  @@size = Vec2(Int32).new 0, 0
+  @@offset = Vec2(Int32).new 0,0
+
+  @@screen_surface : SDL::Surface?
+  @@screen_surface_pointer : Pointer(LibSDL::Surface)?
+
+  @@background_color = SDL::Color.new(255,255,255)
+
+  def create()
+
+  end
+
+  def make_window()
+
+  end
+
+  def clear_screen(r,g,b)
+    if (ba_r = Basil::Renderer.renderer)
+      old_color = ba_r.draw_color
+      ba_r.draw_color = SDL::Color[r,g,b,255]
+      ba_r.clear
+      ba_r.draw_color = old_color
+    end
+    if (surf = Basil::Renderer.surface)
+      surf.fill(@@background_color.r, @@background_color.g, @@background_color.b)
+    end
+  end
+
+  def set_background_color(r,g,b)
+    @@background_color = SDL::Color.new(r,g,b)
+  end
+
+  def surface
+    @@screen_surface
+  end
+
+  def draw_buffer
+
+  end
+
+  def calculate_onscreen_rect
+
+  end
+
+  def present
+    if (ba_r = @@renderer)
+      ba_r.present
+    end
+  end
+
+  def renderer
+    if a == @@renderer
+      a
+    end
+  end
+
+  def window
+    @@window
+  end
+
+  def camera
+    @@camera
+  end
+
+  def size
+    @@size
+  end
+
+  def scale
+    @@scale
+  end
+
+  def current_scale
+    if a == @@renderer
+      a.scale[0]
+    end
+  end
+
+  def fps
+    @@fps
+  end
+
+  def set_fps(set_fps)
+    @@fps = set_fps
+  end
+
+  def offset
+    @@offset
+  end
+
+  extend self
 end
