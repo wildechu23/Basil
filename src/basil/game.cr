815283757ec7d3a6b873bc7b6d3af43e4b6447d4
wildechu <wildechu23@gmail.com>
2019-12-14 02:06:45 -0500

    finished most of game.cr

--- src/basil/game.cr
+++ src/basil/game.cr
@@ -1,3 +1,6 @@
+# TODO: self.game_update
+# Basil::KeyManager
+
 DEBUG_ALPHA = "!+#$%&'()*+,-./" \
               "0123456789:;<=>?" \
               "@ABCDEFGHIJKLMNO" \
@@ -16,10 +19,18 @@ class Basil::Game
   # func runs every loop
   @@loopfunc = Proc(Nil)?
 
+  @@show_hitboxes = false
+
+  @@show_debugger = true
+  @@sort_debugger = false
+  @@debug_string_buffer = [] of String
+
   @@current_fps = 0
+  @@to_destroy = [] of Basil::Object
 
   @@quit = false
 
+  # main loop
   def self.run
     start_time = 0
     
@@ -44,9 +55,101 @@ class Basil::Game
     puts "Exiting..."
     exit
   end
-  
-end
 
-def self.game_update
+  def self.game_update
+    while (event = SDL::Event.poll)
+      case event
+      when SDL::Event::Quit
+        @@quit = true
+      when SDL::Event::Keyboard
+        # TODO: Basil::KeyManager.update(event)
+      when SDL::Event::MouseButtons
+      when SDL::Event::MouseMotion
+      when SDL::Event::MouseWheel
+      else
+        puts "Event not handled: " + event
+      end
+    end
+
+    # Basil::Renderer.camera.update
+
+    if func = @@loopfunc
+      func.call
+    end
+
+    # update objs
+    Basil::Game.loop.each do |objs|
+      objs[1].each do |obj|
+        obj.update
+      end
+    end
+
+    Basil::Renderer.camera.update
+    Basil::Renderer.clear_screen(0,0,0)
+    # Basil::Map.draw
+
+    # draw objs
+    Basil::Game.loop.each do |objs|
+      objs[1].each do |obj|
+        obj.draw
+
+        if @@show_hitboxes
+          if hb = obj.hitbox
+            # Basil::Library.set_draw_color(255,0,0,100)
+            # Basil::Draw.fill_rect(obj.pos.x + hb.pos.x, obj.pos.y + hb.pos.y, hb.size.x.to_i, hb.size.y.to_i)
+          end
+        end
+      end
+    end
+    Basil::Renderer.draw_buffer
+
+    if @@show_debugger
+      Basil::Game.draw_debug
+    end
+
+    Basil::Renderer.present
+    # Basil::KeyManager.clear_pressed
+    Basil::Game.destroy_all_in_buffer
+    # Basil::Library::Mouse.update
+  end
+
+  def self.getfps
+    @@current_fps
+  end
+
+  def self.set_loopfunc(func)
+    @@loopfunc = func
+  end
+
+  def self.show_hitboxes(bool)
+    @@show_hitboxes = bool
+  end
   
+  def self.quit
+    @@quit = true
+  end
+
+  def self.loop
+    @@loop = true
+  end
+
+  def self.debug(string)
+    @@debug_string_buffer << string
+  end
+
+  def self.draw_debug
+  end
+
+  def self.to_destroy
+    @@to_destroy
+  end
+
+  def self.destroy_all_in_buffer
+    @@to_destroy.each do |obj|
+      Basil::Game.loop[obj.class.to_s].delete(obj)
+      if Basil::Game.loop[obj.class.to_s].size == 0
+        # TODO: Delete key
+      end
+    end
+    @@to_destroy = [] of Basil::Object
 end
